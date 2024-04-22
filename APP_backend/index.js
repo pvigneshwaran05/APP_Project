a2aabbb2ffd1fc602347f6a1818591773681786d
Vigneshwaran P <121537862+pvigneshwaran05@users.noreply.github.com>
2024-04-22 14:26:03 +0530

    backend used

---
+++ APP_backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+var name;
+
+app.get("/",(req,res)=>{
+    res.render("login.ejs");
+});
+
+app.get("/home",(req,res)=>{
+    res.render("home.ejs",{name : name});
+});
+
+
+app.post("/greet",(req,res)=>{
+    name = req.body.Username;
+    res.render("greet_page.ejs",{name : name});
+});
+
+
+app.get("/query",(req,res)=>{
+    res.render("query.ejs");
+});
+
+app.listen(port,()=>{
+    console.log("Listening on port "+port);
+})
